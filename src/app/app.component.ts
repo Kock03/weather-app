9b8317cfec7a25717e54f17373b1768ca625b503
VINICIUS KOCK <vinikk03@gmail.com>
2022-10-04 11:41:39 -0300

    weather-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weather-app';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  countries = [
+    {
+      name: "Brazil",
+      cities: ["Rio de Janeiro", "São Paulo", "Blumenau"]
+    },
+
+    {
+      name: "United Kingdom",
+      cities: ["London", "Manchester", "Liverpool"]
+    },
+
+    {
+      name: "United States",
+      cities: ["New York", "Chicago", "Houston"]
+    },
+
+    {
+      name: "Australia",
+      cities: ["Sydney", "Adelaide", "Melbourne"]
+    }
+  ]
+  countryControl!: FormControl;
+  cityControl!: FormControl;
+  cities$!: Observable<string>;
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    this.cityControl = new FormControl("");
+    this.cityControl.valueChanges.subscribe(value => {
+      this.router.navigate([value]);
+    });
+
+    this.countryControl = new FormControl("");
+
+    this.cities$ = this.countryControl.valueChanges.pipe(
+      map(country => country.cities)
+    );
+  }
 }
